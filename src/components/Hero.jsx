03260c96a421b46fc6532fdb29c55f060d7f4795
RikasMRM <rikasrkf@gmail.com>
2023-04-08 13:51:36 +0530

    changed styles

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,25 +2,26 @@ import { motion } from "framer-motion";
 
 import { styles } from "../styles";
 import { ComputersCanvas } from "./canvas";
+import { EarthCanvas } from "./canvas";
 import { logo } from "../assets";
 
 const Hero = () => {
   return (
     <section className={`relative w-full h-screen mx-auto`}>
       <div
-        className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
+        className={`absolute inset-0 top-[220px] z-10 max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
       >
-        <div className='flex flex-col justify-center items-center mt-5'>
+        {/* <div className='flex flex-col justify-center items-center mt-5'>
           <div className='w-5 h-5 rounded-full bg-[#fc674d]' />
           <div className='w-1 sm:h-80 h-40 orange-gradient' />
-        </div>
+        </div> */}
 
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            <span className='text-[#f28c28]'>CAREER</span> EXPO
+            <span className="text-[#f28c28]">CAREER</span> EXPO
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-          Your dream career awaits you 
+            Your dream career awaits you
             {/* <br className='sm:block hidden' /> */}
           </p>
         </div>
@@ -28,11 +29,11 @@ const Hero = () => {
 
       {/* <img src={logo} alt='logo' className='' /> */}
 
-      {/* <ComputersCanvas /> */}
+      {/* <EarthCanvas /> */}
 
-      <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
-        <a href='#about'>
-          <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
+      <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
+        <a href="#about">
+          <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
             <motion.div
               animate={{
                 y: [0, 24, 0],
@@ -42,7 +43,7 @@ const Hero = () => {
                 repeat: Infinity,
                 repeatType: "loop",
               }}
-              className='w-3 h-3 rounded-full bg-secondary mb-1'
+              className="w-3 h-3 rounded-full bg-secondary mb-1"
             />
           </div>
         </a>
