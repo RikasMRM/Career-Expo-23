45cfb3ab185743578c11491c37fd9cb7b84ad016
RikasMRM <rikasrkf@gmail.com>
2023-04-21 00:03:53 +0530

    refactors

--- src/constants/index.js
+++ src/constants/index.js
@@ -188,68 +188,6 @@ const testimonials = [
 ];
 
 const projects = [
-  {
-    name: "1",
-    description:
-      "Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
-    tags: [
-      {
-        name: "react",
-        color: "blue-text-gradient",
-      },
-      {
-        name: "mongodb",
-        color: "green-text-gradient",
-      },
-      {
-        name: "tailwind",
-        color: "pink-text-gradient",
-      },
-    ],
-    image: company1,
-  },
-  {
-    name: "2",
-    description:
-      "Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
-    tags: [
-      {
-        name: "react",
-        color: "blue-text-gradient",
-      },
-      {
-        name: "restapi",
-        color: "green-text-gradient",
-      },
-      {
-        name: "scss",
-        color: "pink-text-gradient",
-      },
-    ],
-    image: company2,
-    source_code_link: "https://github.com/",
-  },
-  {
-    name: "3",
-    description:
-      "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
-    tags: [
-      {
-        name: "nextjs",
-        color: "blue-text-gradient",
-      },
-      {
-        name: "supabase",
-        color: "green-text-gradient",
-      },
-      {
-        name: "css",
-        color: "pink-text-gradient",
-      },
-    ],
-    image: company3,
-    source_code_link: "https://github.com/",
-  },
 ];
 
 const memories = [
@@ -307,7 +245,7 @@ const memories = [
         color: "green-text-gradient",
       },
       {
-        name: "scss",
+        name: "RACSLIIT",
         color: "pink-text-gradient",
       },
     ],
@@ -359,15 +297,15 @@ const memories = [
       "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
     tags: [
       {
-        name: "nextjs",
+        name: "tech",
         color: "blue-text-gradient",
       },
       {
-        name: "supabase",
+        name: "Career Expo",
         color: "green-text-gradient",
       },
       {
-        name: "tech",
+        name: "RACSLIIT",
         color: "pink-text-gradient",
       },
     ],
